20a635fb670b9a4886537ef5610ecb62a8db31ad
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-22 14:26:32 +0100

    Add project structure from start.spring.io

---
+++ src/main/java/gastroinfo/gastroinfo/GastroinfoApplication.java
@@ -0,0 +1,13 @@
+package gastroinfo.gastroinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroinfoApplication.class, args);
+	}
+
+}
