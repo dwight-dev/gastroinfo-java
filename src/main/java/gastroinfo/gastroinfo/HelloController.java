df2ec17a22cfc6339390a8a60d060579b1a33b56
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-22 14:37:31 +0100

    Add example Thymeleaf template

--- src/main/java/gastroinfo/gastroinfo/HelloController.java
+++ src/main/java/gastroinfo/gastroinfo/HelloController.java
@@ -2,14 +2,12 @@ package gastroinfo.gastroinfo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    @ResponseBody
     public String hello() {
-        return "<h1>Hello World!</h1>";
+        return "hello";
     }
 }
