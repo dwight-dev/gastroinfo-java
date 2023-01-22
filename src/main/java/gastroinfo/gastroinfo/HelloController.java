8760fff3b9bc6d20f8c461a3f932e190258e347a
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-22 14:48:08 +0100

    Display current date

--- src/main/java/gastroinfo/gastroinfo/HelloController.java
+++ src/main/java/gastroinfo/gastroinfo/HelloController.java
@@ -1,13 +1,17 @@
 package gastroinfo.gastroinfo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("date", LocalDate.now());
         return "hello";
     }
 }
