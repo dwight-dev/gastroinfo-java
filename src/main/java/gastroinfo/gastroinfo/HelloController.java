01fd458dc5d8f3704655d059273c13c1d195d1e0
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-23 00:19:59 +0100

    Display example data on the home page

--- src/main/java/gastroinfo/gastroinfo/HelloController.java
+++ src/main/java/gastroinfo/gastroinfo/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -12,6 +14,12 @@ public class HelloController {
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("date", LocalDate.now());
+        Map<String, Object> offer = Map.of(
+                "place", Map.of("name", "Some restaurant", "address", "ul. Piotrkowska 123", "phone", "+48123456789"),
+                "description", "Very nice meal",
+                "price", "25.00");
+        Map<String, Object> zone = Map.of("name", "Some zone", "offers", List.of(offer, offer));
+        model.addAttribute("zones", List.of(zone, zone, zone));
         return "hello";
     }
 }
