a28c30dcec3d1391a6d344bf516be3eec62a0b04
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-02-05 00:06:04 +0100

    Allow to specify the date

--- src/main/java/gastroinfo/gastroinfo/HelloController.java
+++ src/main/java/gastroinfo/gastroinfo/HelloController.java
@@ -19,14 +19,16 @@ public class HelloController {
     private final JdbcTemplate jdbc;
 
     @GetMapping("/")
-    public String hello(Model model) {
+    public String hello(Model model, LocalDate date) {
 
-        LocalDate today = LocalDate.now();
-        model.addAttribute("date", today);
-        List<Map<String, Object>> offers = jdbc.queryForList("select offer as description, price, name, address, phone, zone from offers join places on offers.place_id = places.id where date = ?", today);
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        model.addAttribute("date", date);
+        List<Map<String, Object>> offers = jdbc.queryForList("select offer as description, price, name, address, phone, zone from offers join places on offers.place_id = places.id where date = ?", date);
         Map<String, List<Map<String, Object>>> zones = new HashMap<>();
         for (Map<String, Object> offer : offers) {
-            zones.computeIfAbsent((String)offer.get("zone"), (k) -> new ArrayList<>()).add(offer);
+            zones.computeIfAbsent((String) offer.get("zone"), (k) -> new ArrayList<>()).add(offer);
         }
         model.addAttribute("zones", zones.entrySet().stream().map((e) -> Map.of("name", e.getKey(), "offers", e.getValue())));
         return "hello";
